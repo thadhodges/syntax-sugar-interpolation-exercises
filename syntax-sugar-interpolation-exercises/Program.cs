08831e220a3ef3c598051d7097017062edf1d088
Thad <thadisfree@gmail.com>
2021-10-20 10:44:23 -0500

    initial commit

---
+++ syntax-sugar-interpolation-exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace syntax_sugar_interpolation_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write this in different ways...
+            int answer = 4;
+            string response;
+            Console.WriteLine("original code");
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+                Console.WriteLine(response);
+            }
+            else
+            {
+                response = answer + " greater than or equal to nine";
+                Console.WriteLine(response);
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

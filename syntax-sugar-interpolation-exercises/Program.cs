e1aa7bed3c8e1d373ae5bf47ac10f69ab8e58e6f
Thad <thadisfree@gmail.com>
2021-10-20 11:01:57 -0500

    completed ternary inline version of code...

--- syntax-sugar-interpolation-exercises/Program.cs
+++ syntax-sugar-interpolation-exercises/Program.cs
@@ -7,9 +7,9 @@ namespace syntax_sugar_interpolation_exercises
         static void Main(string[] args)
         {
             //write this in different ways...
-            int answer = 4;
+            Console.WriteLine("Original Code");
+            int answer = 9;
             string response;
-            Console.WriteLine("original code");
 
             if (answer < 9)
             {
@@ -18,12 +18,17 @@ namespace syntax_sugar_interpolation_exercises
             }
             else
             {
-                response = answer + " greater than or equal to nine";
+                response = answer + " greater than or equal to 9";
                 Console.WriteLine(response);
             }
+            Console.WriteLine();
+            Console.WriteLine();
 
-
-
+            //Ternary or Inline
+            Console.WriteLine("Ternary or Inline version");
+            var i = 9;
+            var compareToNine = (i > 0 && i != 9) ? $"  {i} is less than 9" : $"  {i} is greater than or equal to 9...";
+            Console.WriteLine(compareToNine);
 
 
 
